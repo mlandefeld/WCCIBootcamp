0c10d6a640f3ab26cc2f01154ecb0b57c3554779
Margaret Landefeld <mlandefeld@gmail.com>
2016-01-13 22:59:54 -0500

    first commit

---
+++ Bootcamp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Week Two Day One 
+            //WeekTwo.DayOne WeekTwoDayOne = new WeekTwo.DayOne();
+            //WeekTwoDayOne.old();
+            
+            //Week Two Day Three - Fizz Buzz
+            //WeekTwo.DayThree WeekTwoDayThree = new WeekTwo.DayThree();
+            //WeekTwoDayThree.fizzBuzz();
+
+            //Week Two Day Three - Factorial Problem
+            //WeekTwoDayThree.multiplyRange();
+
+            //Week Two Day Four - 
+            //WeekTwo.DayFour WeekTwoDayFour = new WeekTwo.DayFour();
+
+        }
+    }
+}
---
+++ Bootcamp/WeekTwo/DayOne.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.WeekTwo
+{
+    class DayOne
+    {
+        public void old()
+        {
+            /* Console.WriteLine("What is your age? ");            
+             int age = int.Parse(Console.ReadLine());
+
+             if (age >= 17)
+             {
+                 Console.WriteLine("You can see the movie!");
+             }
+             else
+             {
+                 Console.WriteLine("You are too young to see the movie.");
+             }
+                
+
+            Console.WriteLine("What is your name? ");
+            Console.ReadLine();
+            string name = Console.ReadLine();
+
+            if (name[0] == 'A')
+            {
+                Console.WriteLine("Go to the front of the Line");
+            }
+            
+            
+
+            Console.WriteLine("How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How tall are you? ");
+            Console.ReadLine();
+            double height = double.Parse(Console.ReadLine());
+
+
+            if (age >= 18 && height >= 5.0)
+            {
+                Console.WriteLine("You are old enough! ");
+            }
+
+            else
+            {
+                Console.WriteLine("You are too young");
+            }
+
+           
+
+            Console.WriteLine("What is your age? ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 16 && age < 18)
+            {
+                Console.WriteLine("You Can Drive!");
+            }
+            else 
+            {
+                if (age >= 18)
+                {
+                    Console.WriteLine("You're an adult!");
+                }
+                 else 
+                {
+                    Console.WriteLine("Your're not an adult yet");
+                }
+            }
+
+            Console.ReadLine();
+    
+
+            Console.WriteLine("What is your Score (number between 1 & 9)? ");
+            int score = int.Parse(Console.ReadLine());
+
+            if (score >= 1 && score <= 3)
+            {
+                Console.WriteLine("You scored " + score + ". You received a bonus of " + ((score * 10) - score) + ". Your total score is " + (score * 10));
+
+            }
+
+            if(score >= 4 && score <= 6)
+            {
+                Console.WriteLine("You scored " + score + ". You received a bonus of " + ((score * 100) - score) + ". Your total score is " + (score *100));
+
+            }
+
+            if(score >= 7 && score <= 9)
+            {
+                Console.WriteLine("You scored " + score + ". You received a bonus of " + ((score * 1000) - score) + ". Your total score is " + (score * 1000));
+
+            }
+
+            else
+            {
+                   Console.WriteLine("Your score is not within range");
+
+            }
+            
+
+            CodeEval
+            string line = "701";
+            int lineAsInt = Convert.ToInt32(line);
+
+            if (lineAsInt % 2 == 0)
+            {
+                Console.WriteLine("1");
+            }
+
+            else
+            {
+                Console.WriteLine("0");
+            }
+            
+
+            //slide 38 on Conditional Statement
+          
+            int input = Convert.ToInt32(Console.ReadLine());
+          
+
+            if (input % 2 == 0 )
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+
+            }
+
+    
+    //slide 40 conditional statements
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c;
+
+            Console.WriteLine("a's value is: " + a);
+            Console.WriteLine("b's value is: " + b);
+
+            if (a>b)
+            {
+                Console.WriteLine(" a is greater than b, swapping their values");
+                c = a;
+                a = b;
+                b = c;
+            }
+            else
+            {
+                Console.WriteLine(" a is less than b, not swapping their values");
+            }
+            Console.WriteLine("a's value is: " + a);
+            Console.WriteLine("b's value is: " + b);
+
+    //slide 41 of Conditional Statements
+            Console.WriteLine("Type three different numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The biggest number is: ");
+            if (a > b)
+            {
+                if(a > c)
+                {
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                }
+            }
+            else
+            {
+                if(b > c)
+                {
+                    Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                }
+            }
+
+            */
+        }
+    }
+}
